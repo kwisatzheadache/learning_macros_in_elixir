ffd31a20742723477c1098c8eaf0c2375dabc428
kwisatzheadache <michaelhorvath87@gmail.com>
2017-04-23 16:42:55 +0200

    inital commit

---
+++ lib/learning_macros_in_elixir.ex
@@ -0,0 +1,18 @@
+defmodule LearningMacrosInElixir do
+  @moduledoc """
+  Documentation for LearningMacrosInElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LearningMacrosInElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/learning_macros_in_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule LearningMacrosInElixirTest do
+  use ExUnit.Case
+  doctest LearningMacrosInElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
